befc2809b1d26fc38254ae70405a621938c30e94
nyahyun <hamnahyun@outlook.com>
2022-03-30 20:32:17 +0900

    props

--- react/02_component/hello-component/src/MyComponent.js
+++ react/02_component/hello-component/src/MyComponent.js
@@ -1,5 +1,26 @@
-const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
-};
+import { Component } from 'react';
+import ProTypes from 'prop-types';
+
+class MyComponent extends Component {
+    static defaultProps = {
+            name: '기본 이름'
+    };
+    static propTypes = {
+        name: ProTypes.string,
+        favoriteNumber: ProTypes.number.isRequired
+    };
+    render() {
+        const { name, favoriteNumber, children } = this.props; // 비구조화 할당
+        return (
+        <div>
+            안녕하세요, 제 이름은 {name}입니다. <br />
+            children 값은 {children}
+            입니다.
+            <br />
+            제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+        </div>
+        );
+    }
+}
 
 export default MyComponent; 
