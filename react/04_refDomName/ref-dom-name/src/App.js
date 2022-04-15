284a380e2193c537444f8392976f8141c7e2ce62
nyahyun <hamnahyun@outlook.com>
2022-04-15 21:32:51 +0900

    Ref exam enter password

---
+++ react/04_refDomName/ref-dom-name/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import VaildationSample from './VaildationSample';
+
+class App extends Component {
+  render() {
+    return (
+      <VaildationSample/>
+    );
+  }
+}
+
+export default App;
