2b12db652be62e2a15149fd8ae80f68a8358e426
nyahyun <hamnahyun@outlook.com>
2022-04-19 20:46:03 +0900

    Component ref

--- react/04_refDomName/ref-dom-name/src/App.js
+++ react/04_refDomName/ref-dom-name/src/App.js
@@ -1,10 +1,16 @@
 import { Component } from "react";
-import VaildationSample from './VaildationSample';
+//import VaildationSample from './VaildationSample';
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
     return (
-      <VaildationSample/>
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨밑으로
+        </button>
+      </div>
     );
   }
 }
