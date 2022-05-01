711703fd08c2e29c729cf48dfcc4594e10c45e79
nyahyun <hamnahyun@outlook.com>
2022-05-01 17:27:29 +0900

    map need key

--- react/05_ComponentRepeat/component-repeact/src/IterationSample.js
+++ react/05_ComponentRepeat/component-repeact/src/IterationSample.js
@@ -1,6 +1,6 @@
 const IterationSample = () => {
     const names = ['눈사람', '얼음', '눈', '바람'];
-    const namelist = names.map(name => <li>{name}</li>);
+    const namelist = names.map((name, index) => <li key={index}>{name}</li>);
     return <ul>{namelist}</ul>;
 };
 
