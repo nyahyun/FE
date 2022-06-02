abeca669e0112587e7c63f662313103256f4a3c2
nyahyun <hamnahyun@outlook.com>
2022-06-03 00:32:16 +0900

    only Execute useEffect when name update

--- react/07_Hooks/hooks-tutorial/src/Info.js
+++ react/07_Hooks/hooks-tutorial/src/Info.js
@@ -3,16 +3,9 @@ import { useState, useEffect } from 'react';
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
-    /*useEffect(() => {
-        console.log('랜더링이 완료되었습니다!');
-        console.log({
-            name,
-            nickname
-        });
-    });*/
     useEffect(() => {
-        console.log('마운트 될 때만 실행됩니다.');
-    }, []);
+        console.log(name);
+    },[name]);
     const onChangeName = e => {
         setName(e.target.value);
     };
