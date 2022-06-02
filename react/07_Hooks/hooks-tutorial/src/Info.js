7794e7417f8d3ca507a150efcfbaa3828903a630
nyahyun <hamnahyun@outlook.com>
2022-06-03 00:42:43 +0900

    useEffect cleanup

--- react/07_Hooks/hooks-tutorial/src/Info.js
+++ react/07_Hooks/hooks-tutorial/src/Info.js
@@ -5,12 +5,12 @@ const Info = () => {
     const [nickname, setNickname] = useState('');
     useEffect(() => {
         console.log('effect');
-        console.log(name);
+        //console.log(name);
         return () => {
             console.log('cleanup');
-            console.log(name);
+            //console.log(name);
         };
-    },[name]);
+    },[/*name*/]);
     const onChangeName = e => {
         setName(e.target.value);
     };
